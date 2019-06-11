891cfd694db29553de28bc3e0fe51a31ffae756d
dima1997 <franki.dimaria@gmail.com>
2019-06-11 08:51:17 -0300

    termino descripcion de clases de de common

--- common/protocol/protocol_code.h
+++ common/protocol/protocol_code.h
@@ -7,8 +7,17 @@
 
 #include <cstdint>
 
-enum CommandName: uint8_t { new_game, join_game };
-enum JoinGameResponse: uint8_t { command_ok, game_is_full, non_existent_game };
+enum CommandName: uint8_t { 
+    new_game, 
+    join_game 
+};
+
+enum JoinGameResponse: uint8_t { 
+    command_ok, 
+    game_is_full, 
+    non_existent_game 
+};
+
 enum EventType: uint8_t {
     object_moves, 
     object_switch_state, 
