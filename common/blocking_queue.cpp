e21d4115666838b0e9b72669e213871616939601
dima1997 <franki.dimaria@gmail.com>
2019-06-20 20:13:56 -0300

    Refactorizo colas, poniendo la definicion en el .h para poder declarar otras tipos de colas en otros modulos

--- common/blocking_queue.cpp
+++ common/blocking_queue.cpp
@@ -12,42 +12,6 @@
 #include <connector/connector.h>
 #include <memory>
 
-template <class T>
-BlockingQueue<T>::BlockingQueue(): mutex(), cv(), queue() {}
-
-template <class T>
-BlockingQueue<T>::~BlockingQueue() = default;
-
-template <class T>
-void BlockingQueue<T>::push(T &element) {
-    std::unique_lock<std::mutex> l(mutex);
-    queue.push(std::move(element));
-    cv.notify_one();
-}
-
-template <class T>
-bool BlockingQueue<T>::pop(T &element) {
-    std::unique_lock<std::mutex> l(mutex);
-    cv.wait(l, [this]{ return !queue.empty() || closed; });
-    if (queue.empty())
-        return false;
-    element = std::move(queue.front());
-    queue.pop();
-//    element = std::move(task);
-    return true;
-}
-
-template <class T>
-void BlockingQueue<T>::close() {
-    std::unique_lock<std::mutex> l(mutex);
-    closed = true;
-    cv.notify_all();
-}
-
-template<class T>
-BlockingQueue<T>::BlockingQueue(BlockingQueue<T> &&other) noexcept:
-                                mutex(), cv(), queue(std::move(other.queue)), closed(other.closed) {}
-
 // To use this template just add here, for example:
 template class BlockingQueue<Connector>;
 template class BlockingQueue<std::shared_ptr<Event>>;
--- common/thread_safe_queue.h
+++ common/thread_safe_queue.h
@@ -4,14 +4,12 @@
 #include <queue>
 #include <mutex>
 
-#include <memory>
-#include <protocol/event/object_moves_event.h>
-
 template <class T>
 class ThreadSafeQueue {
 private:
     std::mutex mutex;
     std::queue<T> queue;
+
 public:
     /*
     Inicializa una cola segura en hilos de ejecucion,
@@ -19,17 +17,13 @@ public:
     T debe implementar constructor y asignacion por 
     movimiento semantico.
     */
-    ThreadSafeQueue();
-
-    ThreadSafeQueue(ThreadSafeQueue<T> &&other) noexcept;
-
-    ThreadSafeQueue<T> &operator=(ThreadSafeQueue<T> &&other) noexcept;
+    ThreadSafeQueue(): mutex(), queue() {}
 
     /*
     Destruye un la cola segura en hilos de ejecucion,
     no bloqueante. 
     */
-    ~ThreadSafeQueue();
+    ~ThreadSafeQueue() = default;
 
     /*
     PRE: Recibe una referencia de un elemento de tipo T
@@ -38,7 +32,10 @@ public:
     en la cola.
     El elemento recibo queda en esta nulo.
     */
-    void push(T &element);
+    void push(T &element) {
+        std::unique_lock<std::mutex> l(this->mutex);
+        this->queue.push(std::move(element));
+    }
 
     /*
     PRE: Recibe una referencia a un elemento de tipo T
@@ -48,18 +45,23 @@ public:
     primero, devolviendo true; o false, si no se desencolo 
     nada puesto que la cola esta vacia.
     */
-    bool pop(T &element);
+    bool pop(T &element) {
+        std::unique_lock<std::mutex> l(this->mutex);
+        if (this->queue.empty())
+            return false;
+        T task = std::move(queue.front());
+        queue.pop();
+        element = std::move(task);
+        return true;
+    }
+
+    ThreadSafeQueue(ThreadSafeQueue<T> &&other) noexcept:
+                                    mutex(), queue(std::move(other.queue)) {}
+
+    ThreadSafeQueue<T> &operator=(ThreadSafeQueue<T> &&other) noexcept {
+        this->queue = std::move(other.queue);
+        return *this;
+    }
 };
-
-enum gameObjectAction_t : uint32_t {
-    MOVE_LEFT,
-    MOVE_RIGHT
-};
-
-typedef ThreadSafeQueue<std::unique_ptr<ObjectMovesEvent>>
-TSQueueChangesMade_t;
-
-typedef ThreadSafeQueue<std::pair<uint32_t, gameObjectAction_t>> 
-TSQueueChangesAsk_t;
 
 #endif // THREAD_SAFE_QUEUE_H
