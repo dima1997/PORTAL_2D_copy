e74113f1bc2ce9ee3734cfc8837b34d247d3261a
dima1997 <franki.dimaria@gmail.com>
2019-06-21 16:29:45 -0300

    La refactorizacion de MapCreator y TextureFactory es un exito; las chells tienen color

--- client/includes/window/map_creator.h
+++ client/includes/window/map_creator.h
@@ -21,67 +21,7 @@ public:
 
     /*Destruye el creador de mapa*/
     ~MapCreator();
-    /*
-    void add_block_metal(uint32_t id, Area area);
-
-    void add_block_rock(uint32_t id, Area area);
-
-    void add_block_acid(uint32_t id, Area area);
-
-    void add_portal_blue(uint32_t id, Area area);
-
-    void add_portal_orange(uint32_t id, Area area);
-
-    void add_door_one(uint32_t id, Area area);
-
-    void add_button(uint32_t id, Area area);
-
-    void add_rock_one(uint32_t id, Area area);
-
-    void add_rock_two(uint32_t id, Area area);
-
-    void add_rock_three(uint32_t id, Area area);
-
-    void add_rock_four(uint32_t id, Area area);
-
-    void add_rock_five(uint32_t id, Area area);
-
-    void add_rock_six(uint32_t id, Area area);
-
-    void add_rock_seven(uint32_t id, Area area);
-
-    void add_rock_eight(uint32_t id, Area area);
-
-    void add_barrier(uint32_t id, Area area);
-
-    void add_energy_ball_green(uint32_t id, Area area);
-
-    void add_receiver(uint32_t id, Area area);
-
-    void add_emitter_right(uint32_t id, Area area);
-
-    void add_emitter_up(uint32_t id, Area area);
-
-    void add_emitter_left(uint32_t id, Area area);
-
-    void add_emitter_down(uint32_t id, Area area);
-
-    void add_triangle_botom_left(uint32_t id, Area area);
-
-    void add_triangle_botom_right(uint32_t id, Area area);
-
-    void add_triangle_top_left(uint32_t id, Area area);
-
-    void add_triangle_top_right(uint32_t id, Area area);
-
-    void add_cake(uint32_t id, Area area);
-
-    void add_chell(uint32_t id, Area area);
-
-    void add_background(uint32_t id, Area area);
-
-    void add_record(uint32_t id, Area area);
-    */
+    
     void add_texture(uint32_t id, Area area, std::string & subSectionName);
 
     void add_texture(
