ea11f36d2e5b0f596567c99d76c3edb7161b2a66
Francisco Sicardi <francisco.sicardi@despegar.com>
2019-06-25 17:36:44 -0300

    server main

--- server/main.cpp
+++ server/main.cpp
@@ -3,17 +3,24 @@
 //
 
 #include <string>
+#include <iostream>
 #include "server/server.h"
 #include "config/global_configuration.h"
 
 int main(int argc, char **argv) {
-    std::string port = CONFIG.defaultPort;
-    if (argc == 2) {
-        port = argv[1];
-    } else if (argc > 2) {
+    try {
+        std::string port = CONFIG.defaultPort;
+        if (argc == 2) {
+            port = argv[1];
+        } else if (argc > 2) {
+            return 1;
+        }
+        Server server(port);
+        server();
+        return 0;    
+    } catch (const std::exception &s) {
+        std::cerr << s.what() << std::endl;
         return 1;
-    }
-    Server server(port);
-    server();
-    return 0;
+    } 
+    
 }
